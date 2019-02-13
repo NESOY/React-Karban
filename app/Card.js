32c63f61e453fe790301b3b4daeca1aceaaa88f9
Kwon Young Jae <kyoje11@gmail.com>
2019-02-13 18:11:26 +0900

    Update Toggle Card Function

--- app/Card.js
+++ app/Card.js
@@ -2,15 +2,34 @@ import React, { Component } from 'react';
 import CheckList from './CheckList'
 
 class Card extends Component {
+    constructor(){
+        super(...arguments)
+        this.state = {
+            showDetails: false
+        };
+    }
+
+    toggleDetails(){
+        this.setState({showDetails: !this.state.showDetails})
+    }
+
     render(){
-        return(
-            <div className="card">
-                <div className="card__title">{this.props.title}</div>
+        let cardDetails;
+        if(this.state.showDetails){
+            cardDetails = (
                 <div className="card__details">
                     {this.props.description}
-
                     <CheckList cardId={this.props.id} tasks={this.props.tasks}></CheckList>
                 </div>
+            )
+        }
+        return(
+            <div className="card">
+                <div className={ this.state.showDetails ? "card__title--is-open" : "card__title"} onClick={this.toggleDetails.bind(this)}>
+                {/* <div className="card__title" onClick={this.toggleDetails.bind(this)}> */}
+                    {this.props.title}
+                </div>
+                {cardDetails}
             </div>
         );
     }
