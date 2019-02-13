e986cc79ac1aa0e709f76261e13f819cb2a7fd7b
Kwon Young Jae <kyoje11@gmail.com>
2019-02-13 19:06:57 +0900

    Update Proptypes

--- app/CheckList.js
+++ app/CheckList.js
@@ -1,9 +1,11 @@
-import React, { Component } from 'react';
+import React, { Component, PropTypes } from 'react';
 
+// 제어 컴포넌트
+// 값이나 확인되는 속성을 가지는 폼 컴포넌트
 class CheckList extends Component {
     render(){
         let tasks = this.props.tasks.map((task) => {
-            return <li className="checklist__task">
+            return <li key={task.id} className="checklist__task">
                 <input type="checkbox" defaultChecked={task.done}/>
                 {task.name}
                 <a href="#" className="checklist__task--remove"/>
@@ -13,9 +15,14 @@ class CheckList extends Component {
         return(
             <div className="checklist">
                 <ul>{tasks}</ul>
+                <input type="text" className="checklist--add-task" placeholder="Type then hit Enter to add a task"/>
             </div>
         );
     }
 }
 
+CheckList.propTypes = {
+    cardId: PropTypes.number,
+    tasks: PropTypes.arrayOf(PropTypes.object)
+}
 export default CheckList;
