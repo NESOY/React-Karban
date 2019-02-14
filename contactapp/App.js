1cc21e06b21026b3ec3844827d0abc1d4ab7dc6a
Kwon Young Jae <kyoje11@gmail.com>
2019-02-14 15:00:09 +0900

    Update Api Get json Data

--- contactapp/App.js
+++ contactapp/App.js
@@ -2,35 +2,7 @@ import React from 'react';
 import {
     render,
 } from 'react-dom';
-import ContactApp from './ContactApp';
+import ContactsAppContainer from './ContactsAppContainer';
 
-const contacts = [{
-        name: '권영재',
-        email: 'cassiozen@gmail.com',
-    },
-    {
-        name: 'Dan Abramov',
-        email: 'gaearon@somewhere.com',
-    },
-    {
-        name: 'Pete Hunt',
-        email: 'floydophone@somewhere.com',
-    },
-    {
-        name: 'Paul O’Shannessy',
-        email: 'zpao@somewhere.com',
-    },
-    {
-        name: 'Ryan Florence',
-        email: 'rpflorence@somewhere.com',
-    },
-    {
-        name: 'Sebastian Markbage',
-        email: 'sebmarkbage@here.com',
-    },
-];
 
-render( < ContactApp contacts = {
-            contacts
-        }
-        />, document.getElementById('root'));
+render( < ContactsAppContainer / > , document.getElementById('root'));
---
+++ contactapp/ContactsAppContainer.js
@@ -0,0 +1,40 @@
+import React, {
+    Component
+} from 'react';
+import {
+    render
+} from 'react-dom';
+import 'whatwg-fetch';
+import ContactsApp from './ContactApp'
+
+class ContactsAppContainer extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            contacts: []
+        };
+    }
+
+    componentDidMount() {
+        fetch('./contacts.json')
+            .then((response) => response.json())
+            .then((responseData) => {
+                this.setState({
+                    contacts: responseData
+                })
+            })
+            .catch((error) => {
+                console.log('Error fetching and parsing data', error);
+            })
+    }
+
+    render() {
+        return ( < ContactsApp contacts = {
+                this.state.contacts
+            }
+            />
+        );
+    }
+}
+
+export default ContactsAppContainer;
